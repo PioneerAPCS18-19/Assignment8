1af55c9a08fc7629914f9a4217810392459b5dd0
codytheking <codytheking93@gmail.com>
2018-09-07 10:59:48 -0700

    starter code

---
+++ CheckingAccount.java
@@ -0,0 +1,174 @@
+/**
+ * CheckingAccount models a bank account and can
+ * simulate time passing, as well as taking
+ * deposits and withdrawls.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class CheckingAccount
+{
+    private double balance;
+    private double rate;  // as decimal, compounds monthly
+    private String owner;
+    private int acctAge;  // in months
+
+    /**
+     * Constructor
+     *
+     * @param double for balance, double for rate, String for account owner
+     *
+     * Create account with starting balance a, interest rate r, owner own
+     * Remember to initialize all class variables.
+     */
+    
+    // your implementation...
+
+
+    
+    /**
+     * Constructor
+     *
+     * @param String for account owner
+     *
+     * Create account with owner given, but without an initial deposit.
+     * Remember to initialize all class variables (set rate = 0.001).
+     */
+    
+    // your implementation...
+
+
+
+    /**
+     * getBalance
+     * 
+     * @param none
+     * @return returns the current balance in the account as a double
+     */
+
+    // your implementation...
+
+
+    
+    /**
+     * getOwner
+     * 
+     * @param none
+     * @return returns the owner of the account as a string
+     */
+    
+    // your implementation...
+
+
+    
+    /**
+     * getRate
+     * 
+     * @param none
+     * @return returns the interest rate for the account
+     */
+    
+    // your implementation...
+
+
+    
+    /**
+     * getAge
+     * 
+     * @param none
+     * @return returns the current age of the account
+     */
+    
+    // your implementation...
+    
+
+    
+    /**
+     * processDeposit
+     *
+     * @param amount to be deposited as double
+     * @return none
+     *
+     * Amount is added (deposited) to current balance
+     */
+    
+    // your implementation...
+    
+
+    
+    /**
+     * processWithdrawl
+     *
+     * @param amount to be withdrawn as double
+     * @return none
+     * 
+     * Amount is deducted (withdrawn) from the current balance
+     */ 
+    
+    // your implementation...
+
+
+    
+    /**
+     * runMonth
+     *
+     * @param none
+     * @return none
+     *
+     * A month is simulated (passed). Interest is added to balance
+     * and acctAge is incremented by one.
+     * 
+     * A = P * (1 + (rate / 12))
+     * 
+     * P is current ammount
+     * A is new amount after interest is added.
+     */
+    
+    // your implementation...
+     
+
+
+
+
+
+
+
+
+
+    /** DO NOT EDIT BELOW HERE */
+
+
+
+
+    /**
+     * runNMonths
+     *
+     * @param number of months to simulate as an int
+     * @return none
+     *
+     * N months are simulated (passed).
+     */
+    public void runNMonths(int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            runMonth();
+        }
+    }
+
+    /**
+     * printInfo
+     *
+     * @param none
+     * @return account info (age, owner, rate, balance) is shown.
+     */
+    public void printInfo()
+    {
+        System.out.println("Checking Account (" + getAge() + " months old)");
+        System.out.println("Rate: " + (getRate() * 100) + "%");
+        System.out.println("Owner: " + getOwner());
+        double b = Math.round(getBalance() * 100.0) / 100.0;
+        System.out.println("Balance: $" + b + "\n");
+    }
+}
---
+++ Runner.java
@@ -0,0 +1,44 @@
+/**
+ * This class runs the program for managing a checking account.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AccountRunner
+{
+    public static void main(String[] args)
+    {
+        // create checkingAccount object with starting deposit of
+        // $1000, 1.2% interest rate, and an account owner.
+
+        
+        // create checkingAccount object with an account owner (no initial deposit).
+
+        
+        // print account info for both accounts
+
+        
+        // make one month pass for both accounts
+
+        
+        // print account info for both accounts
+
+        
+        // make three months pass for both accounts
+        
+        
+        // deposit $300 into both accounts
+        
+        
+        // make eight months pass for both accounts
+        
+        
+        // print account info for both accounts
+        
+        
+        // withdraw $75 from both accounts
+        
+        
+        // print account info for both accounts
+    }
+}
